3b27373169cc7906863c6b0527e3d24dfcd98964
irizaram <irizaram@miamioh.edu>
2016-05-08 23:19:04 -0400

    Added files via upload

---
+++ Reciever_Code_Final/Reciever_Code_Final.ino
@@ -0,0 +1,89 @@
+//Reciever Code
+
+//Libraries
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+
+//Initialize Variables and Transciever Code
+int msg[1];
+RF24 radio(9,10);
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+int allClearLED = 6;
+int landLED = 5;
+int emergLED = 3;
+int motorPin = 7;
+int count = 0;
+
+//Setup Void Code
+void setup(void){
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(1,pipe);
+  radio.startListening();
+  pinMode(allClearLED, OUTPUT);
+  pinMode(landLED, OUTPUT);
+  pinMode(emergLED, OUTPUT);
+}
+
+//Void Loop Code
+void loop(void){
+
+//Checks for radio connection
+ if (radio.available()){
+   bool terminate = false;  
+   
+   //Always check for a command to be given from the transmitter
+   while (!terminate){
+     
+     //Read the transmission being given from the transmiter
+     terminate = radio.read(msg, 1);      
+     
+     //If the "emergency protocols" button has been pressed
+     if (msg[0] == 33 ){
+       Serial.println("WARNING! TERMINATION PROTOCOLS ACTIVATED! COMMENCING EMERGENCY SHUTDOWN!");
+       digitalWrite(allClearLED, LOW);
+       digitalWrite(landLED, LOW);
+       digitalWrite(emergLED,HIGH);
+       digitalWrite(motorPin, LOW); 
+       delay(100);
+       digitalWrite(emergLED,LOW);
+       terminate = true;   
+       delay(100);
+     }
+     
+     //If the "landing" command has been given
+     else if(msg[0] == 66){
+       Serial.println("Landing protocols activated. Now landing the quadcopter");
+       digitalWrite(motorPin, HIGH);
+       digitalWrite(allClearLED, LOW);
+       digitalWrite(landLED, HIGH);
+       digitalWrite(emergLED,LOW);
+       delay(1);
+       digitalWrite(motorPin, LOW);
+       delay(199);
+     }
+     
+     //If the "all clear" command has been given
+     else if(msg[0] == 99){
+       Serial.println("All systems normal");
+       digitalWrite(allClearLED, HIGH);
+       digitalWrite(landLED, LOW);
+       digitalWrite(emergLED,LOW);
+       digitalWrite(motorPin, HIGH);
+       delay(200);
+     }
+     }
+   }
+ //If there is no detection of the transmitter, the reciever will shut down the motor
+ else{
+   Serial.println("WARNING! LOST CONNECTION WITH TRANSMITTER! COMMENCING EMERGENCY SHUTDOWN!");
+   digitalWrite(allClearLED, HIGH);
+   digitalWrite(landLED, HIGH);
+   digitalWrite(emergLED, HIGH);
+   digitalWrite(motorPin, LOW);
+   delay(200);
+ }
+}
+
+
---
+++ Transmitter_Code_Final/Transmitter_Code_Final.ino
@@ -0,0 +1,80 @@
+  //Transmitter Code
+  
+  //Libraries
+  #include  <SPI.h>
+  #include "nRF24L01.h"
+  #include "RF24.h"
+  
+  //Initialize variables and Transciever Code
+  int msg[1];
+  RF24 radio(9,10);
+  const uint64_t pipe = 0xE8E8F0F0E1LL;
+  int allClearSwitch = 6;
+  int landSwitch = 7;
+  int killSwitch = 8; 
+  boolean killStatus = false;
+  boolean clearStatus = true;
+  boolean landStatus = false;
+  
+  //Setup Void Code
+  void setup(void){
+    Serial.begin(9600);
+    radio.begin();
+    radio.openWritingPipe(pipe);
+  }
+  
+  //Loop Void Code
+  void loop(void){
+    
+   //If the termination switch is pushed down
+   if (digitalRead(killSwitch) == HIGH){
+     
+     //Continuously transmit the "emergency protocol" command
+     clearStatus = false;
+     landStatus = false;
+     killStatus = true;
+     delay(100);
+   }
+   
+   //If the landing switch is pushed down
+   else if (digitalRead(landSwitch) == HIGH){
+     
+     //Continuously transmit the "landing" command
+     clearStatus = false;
+     landStatus = true;
+     killStatus = false;
+     delay(100);
+   }
+   
+   //If the landing switch is pushed down
+   else if (digitalRead(allClearSwitch) == HIGH){
+     
+     //Continuously transmit the "all clear" command
+     clearStatus = true;
+     landStatus = false;
+     killStatus = false;
+     delay(100);
+   }
+   
+   
+   //Determinne the kind of signal to transmit
+   
+   //-If the "emergency protocol" command has been given
+   if(killStatus == true){
+     msg[0] = 33;
+   }
+   
+   //-If the "landing" command has been given
+   else if(landStatus == true){
+     msg[0]=66;
+   }
+   
+   //-If the "all clear" command has been given
+   else if(clearStatus == true){
+     msg[0]=99;
+   }
+   
+   //Transmit the coommand to the receiver
+   radio.write(msg, 1);
+ }
+
